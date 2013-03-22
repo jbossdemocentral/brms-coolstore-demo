2faf3b7083d1a0352fcb8dd4577007bfbd46b173
Eric D. Schabell <eric@schabell.org>
2013-03-22 16:25:02 +0100

    Added mavenizing local repo with brms product deps.

--- init.sh
+++ init.sh
@@ -4,15 +4,28 @@ JBOSS_HOME=./target/jboss-eap-6.0
 SERVER_DIR=$JBOSS_HOME/standalone/deployments/
 SERVER_CONF=$JBOSS_HOME/standalone/configuration/
 LIB_DIR=./support/lib
+SUPPORT_DIR=./support
 SRC_DIR=./installs
 EAP=jboss-eap-6.0.1.zip
 BRMS=brms-p-5.3.1.GA-deployable-ee6.zip
 EAP_REPO=jboss-eap-6.0.1-maven-repository
+MVN_VERSION=5.3.1.BRMS
 VERSION=5.3.1
 
 echo
 echo "Setting up the ${DEMO} environment..."
 echo
+
+command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
+
+installPom() {
+	    mvn -q install:install-file -Dfile=../$SUPPORT_DIR/$2-$MVN_VERSION.pom.xml -DgroupId=$1 -DartifactId=$2 -Dversion=$MVN_VERSION -Dpackaging=pom;
+}
+
+installBinary() {
+			unzip -q $2-$MVN_VERSION.jar META-INF/maven/$1/$2/pom.xml;
+			mvn -q install:install-file -DpomFile=./META-INF/maven/$1/$2/pom.xml -Dfile=$2-$MVN_VERSION.jar -DgroupId=$1 -DartifactId=$2 -Dversion=$MVN_VERSION -Dpackaging=jar;
+}
 
 # make some checks first before proceeding.	
 if [[ -r $SRC_DIR/$EAP || -L $SRC_DIR/$EAP ]]; then
@@ -116,6 +129,64 @@ cp support/standalone.xml $SERVER_CONF
 echo "  - making sure standalone.sh for server is executable..."
 echo
 chmod u+x $JBOSS_HOME/bin/standalone.sh
+
+echo
+echo Installing the BRMS binaries into the Maven repository...
+echo
+
+unzip -q $SRC_DIR/$BRMS jboss-brms-engine.zip
+unzip -q jboss-brms-engine.zip binaries/*
+unzip -q $SRC_DIR/$BRMS jboss-jbpm-engine.zip
+unzip -q -o -d ./binaries jboss-jbpm-engine.zip
+cd binaries
+
+echo Installing parent POMs...
+echo
+installPom org.drools droolsjbpm-parent
+installPom org.drools droolsjbpm-knowledge
+installPom org.drools drools-multiproject
+installPom org.drools droolsjbpm-tools
+installPom org.drools droolsjbpm-integration
+installPom org.drools guvnor
+installPom org.jbpm jbpm
+
+echo Installing Drools binaries...
+echo
+# droolsjbpm-knowledge
+installBinary org.drools knowledge-api
+# drools-multiproject
+installBinary org.drools drools-core
+installBinary org.drools drools-compiler
+installBinary org.drools drools-jsr94
+installBinary org.drools drools-verifier
+installBinary org.drools drools-persistence-jpa
+installBinary org.drools drools-templates
+installBinary org.drools drools-decisiontables
+# droolsjbpm-tools
+installBinary org.drools drools-ant
+# droolsjbpm-integration
+installBinary org.drools drools-camel
+# guvnor
+installBinary org.drools droolsjbpm-ide-common
+
+echo Installing jBPM binaries...
+echo
+installBinary org.jbpm jbpm-flow
+installBinary org.jbpm jbpm-flow-builder
+installBinary org.jbpm jbpm-persistence-jpa
+installBinary org.jbpm jbpm-bam
+installBinary org.jbpm jbpm-bpmn2
+installBinary org.jbpm jbpm-workitems
+installBinary org.jbpm jbpm-human-task
+installBinary org.jbpm jbpm-test
+
+cd ..
+rm -rf binaries
+rm jboss-brms-engine.zip
+rm jboss-jbpm-engine.zip
+
+echo Installation of binaries "for" BRMS $MVN_VERSION complete.
+echo
 
 echo
 echo You will need to add to your personal ~/.m2/settings.xml the settings provided in the file:
