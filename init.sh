6cd25daa73b5d843a1485678783b5a9abbc557c2
Eric D. Schabell <eric@schabell.org>
2014-03-19 09:15:56 +0100

    Fix to init.sh to remove usage of double [[ for if statements, does not work on Ubuntu shells.

--- init.sh
+++ init.sh
@@ -42,7 +42,7 @@ echo
 command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
 
 # make some checks first before proceeding.	
-if [[ -r $SRC_DIR/$EAP || -L $SRC_DIR/$EAP ]]; then
+if [ -r $SRC_DIR/$EAP ] || [-L ${SRC_DIR/$EAP} ]; then
 		echo EAP sources are present...
 		echo
 else
