7b10ee68da62b9a09e9030dfeb04357ca2342d38
Andrew Block <andy.block@gmail.com>
2014-12-16 17:04:19 -0600

    Updated bash installation script

--- init.sh
+++ init.sh
@@ -65,6 +65,11 @@ echo Product installer running now...
 echo
 java -jar $SRC_DIR/$BRMS $SUPPORT_DIR/installation-brms -variablefile $SUPPORT_DIR/installation-brms.variables
 
+if [ $? -ne 0 ]; then
+	echo Error occurred during $PRODUCT installation!
+	exit
+fi
+
 echo
 echo "  - enabling demo accounts role setup in application-roles.properties file..."
 echo
